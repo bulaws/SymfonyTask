ff932934762a096bd1929f6c2193e23fd17d67fd
bulaws <tarambulaws@ukr.net>
2018-06-29 21:55:26 +0300

    add Welcome Controller

---
+++ src/Controller/WelcomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class WelcomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="welcome")
+     */
+    public function index()
+    {
+        return $this->render('welcome/index.html.twig');
+    }
+}
