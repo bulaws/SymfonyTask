aadf29a5113105e77a9e002c7da51a34868015a0
bulaws <tarambulaws@ukr.net>
2018-07-26 11:15:30 +0300

    Error var response not defind

--- src/Controller/RepositoryInfoController.php
+++ src/Controller/RepositoryInfoController.php
@@ -6,16 +6,17 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use GuzzleHttp\Client;
 use GuzzleHttp\Exception\RequestException;
 
-
 class RepositoryInfoController extends AbstractController
 {
     public function index(string $userName, string $repositoryName)
     {
         $client = new Client();
-        $endpoint = 'https://github.com';
+        $endpoint = 'https://api.github.com/repos';
+        $commits = 'commits';
+
         try {
             $response = $client->request('GET', \sprintf('%s/%s.json', $endpoint, $userName,
-                $repositoryName));
+                $repositoryName, $commits));
         } catch (RequestException $e) {
             if ($e->getResponse()->getStatusCode() === 404) {
                 return $this->render('repositoryInfo/repositoryInfo.html.twig', [
@@ -26,11 +27,7 @@ class RepositoryInfoController extends AbstractController
         var_dump($response);
         die();
         $json = (string) $response->getBody();
-        $repositoryInfo = \json_decode($json, true);
-        var_dump($repositoryInfo);
 
-        return $this->render('repositoryInfo/repositoryInfo.html.twig', [
-
-        ]);
+        return $this->render('repositoryInfo/repositoryInfo.html.twig');
     }
 }
